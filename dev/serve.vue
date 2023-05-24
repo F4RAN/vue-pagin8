910a09c9267e51fb5631ab1c8d9b6409ce03d059
F4RAN <parsa.hassani30@gmail.com>
2023-05-25 03:22:46 +0330

    published files

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VuePagin8 from '@/vue-pagin8.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuePagin8
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-pagin8 />
+  </div>
+</template>
